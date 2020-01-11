2bb702623dfaeb57849af0df374d725ae2d6a681
ZorianThornton5 <zoriant15@gmail.com>
2020-01-11 09:33:24 -0800

    Initial model built and validated.

---
+++ src/model.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import numpy as np
+
+from scipy import sparse
+from matplotlib import pyplot as plt
+
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+random.seed(5)
+# Load in data matrix and functional scores
+bmap = sparse.load_npz("../data/dms_simulation_150000_variants.npz")
+func_scores = np.loadtxt("../data/dms_simulation_150000_variants_funcscores.txt",
+                         delimiter='\t')
+
+# Convert bmap to dataframe
+bmap = bmap.toarray()
+
+
+class Net(nn.Module):
+
+    def __init__(self, input_size, hidden1_size):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden1_size)  # input -> hidden layer
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden1_size, 1)  # hidden -> output layer
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        return out
+
+
+def make_train_test_split(data, labels, train_prop=0.8):
+    """ Create train-test splits from a dataset.
+
+    Args:
+        - data (ndarray): Input dataset as a ndarray
+        - labels (ndarray): Corresponding labels for data.
+        - test_prop (float): Decimal representing the proportion of obs to be
+                             used in the test set. Default is 80%.
+
+    Returns:
+        - X_train (ndarray): The training dataset.
+        - X_test (ndarray): The testing dataset.
+        - y_train (ndarray): The training labels.
+        - y_test (ndarray): The testing labels.
+    """
+    sample_space_size = data.shape[0]
+    idx = np.random.randint(sample_space_size,
+                            size=int(np.ceil(sample_space_size*train_prop)))
+    # Create testing set
+    X_train = data[idx, :]
+    y_train = labels[idx]
+
+    # Create testing set
+    X_test = data[-idx, :]
+    y_test = labels[-idx]
+
+    return X_train, y_train, X_test, y_test
+
+
+# Initialize model
+net = Net(input_size=bmap.shape[1], hidden1_size=1)
+print(net)
+
+# Define loss criterion and optimization method
+criterion = torch.nn.MSELoss()  # MSE loss function
+optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+
+# Create training and testing sets for epochs
+X_train, y_train, X_test, y_test = make_train_test_split(data=bmap,
+                                                         labels=func_scores)
+
+n_epochs = 10
+batch_size = 128
+losses = []
+
+for epoch in range(n_epochs):
+    permutation = np.random.permutation(bmap.shape[0])
+
+    for i in range(0, bmap.shape[0], batch_size):
+        optimizer.zero_grad()
+        id = permutation[i:i+batch_size]
+        batch_X, batch_y = bmap[id], func_scores[id]
+
+        batch_X = torch.from_numpy(batch_X).float()
+        batch_y = torch.from_numpy(batch_y).float()
+
+        # Train the model
+        outputs = net(batch_X)
+        loss = criterion(outputs.squeeze(), batch_y)
+
+        # Backprop and SGD step
+        loss.backward()
+        optimizer.step()
+
+        if (i % batch_size == 0):
+            losses.append(loss.item())
+
+x = np.array(list(range(len(losses))))
+losses = np.array(losses)
+
+plt.plot(x, losses)
+plt.xlabel('Batch ID')
+plt.ylabel('Loss (MSE)')
+plt.title('Training Loss by Batch Size')
+plt.grid(True)
+plt.show()
+
+# Validation
+y_test_var = np.var(y_test)
+net.eval()
+X_test = torch.from_numpy(X_test).float()
+y_test = torch.from_numpy(y_test).float()
+y_pred = net(X_test)
+after_train = criterion(y_pred.squeeze(), y_test)
+
+print('Test loss after Training:', after_train.item())
+print('Variation in test labels:', y_test_var)
---
+++ src/simulate.py
@@ -0,0 +1,112 @@
+import collections
+import itertools
+import random
+import tempfile
+import time
+import warnings
+
+import Bio.Seq
+
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from scipy import sparse
+
+from plotnine import *
+
+import dms_variants.binarymap
+import dms_variants.codonvarianttable
+import dms_variants.simulate
+import dms_variants.utils
+from dms_variants.constants import CBPALETTE, CODONS_NOSTOP
+
+seed = 1  # random number seed
+genelength = 300  # gene length in codons
+libs = ['lib_1']  # distinct libraries of gene
+variants_per_lib = 500 * genelength  # variants per library
+avgmuts = 2.5  # average codon mutations per variant
+bclen = 16  # length of nucleotide barcode for each variant
+variant_error_rate = 0.005  # rate at which variant sequence mis-called
+avgdepth_per_variant = 200  # average per-variant sequencing depth
+lib_uniformity = 5  # uniformity of library pre-selection
+noise = 0.02  # random noise in selections
+bottleneck = 10  # bottleneck from pre- to post-selection, average number of each variant that gets through
+
+# Set seed for reproducibility
+random.seed(seed)
+
+
+# Simulate wildtype sequence
+geneseq = ''.join(random.choices(CODONS_NOSTOP, k=genelength))
+print(f"Wildtype gene of {genelength} codons:\n \n{geneseq}")
+
+# Wildtype protein sequence
+protseq = dms_variants.utils.translate(geneseq)
+print(f"Wildtype protein sequence:\n \n{protseq}")
+
+
+# Create codon variant table
+variants = dms_variants.simulate.simulate_CodonVariantTable(
+        geneseq=geneseq,
+        bclen=bclen,
+        library_specs={lib: {'avgmuts': avgmuts,
+                             'nvariants': variants_per_lib}
+                             for lib in libs},
+        seed=seed
+        )
+
+# Print a few rows from the table
+print(variants.barcode_variant_df.head(n=5))
+
+# Simulate phenotype
+phenosimulator = dms_variants.simulate.SigmoidPhenotypeSimulator(
+        geneseq, seed=seed)
+
+# Simulate variant counts
+counts = dms_variants.simulate.simulateSampleCounts(
+        variants=variants,
+        phenotype_func=phenosimulator.observedEnrichment,
+        variant_error_rate=variant_error_rate,
+        pre_sample={'total_count': int(variants_per_lib * avgdepth_per_variant),
+                    'uniformity': lib_uniformity},
+        pre_sample_name='pre-selection',
+        post_samples={'post-selection':
+                        {'noise': noise,
+                         'total_count': int(variants_per_lib * avgdepth_per_variant),
+                         'bottleneck': int(bottleneck * variants_per_lib)}
+                        },
+        seed=seed
+        )
+
+# View the variants
+print(counts.head(n=5))
+
+# Add counts to variants table
+variants.add_sample_counts_df(counts)
+print(variants.n_variants_df())
+
+# Assign functional scores
+func_scores = variants.func_scores('pre-selection', libraries=libs)
+
+# Print functional scores dataframe
+print(func_scores.head(n=5).round(3))
+
+# Create Binary Map
+bmap = dms_variants.binarymap.BinaryMap(func_scores,
+                                        expand=True,
+                                        wtseq=protseq)
+bmap_df = bmap.binary_variants.toarray()
+
+# Take a look at the variants and corresponding functional scores
+print("Binary Variants: ")
+print(bmap_df[0:5])
+print("\n")
+print("Functional Scores: \n")
+print(bmap.func_scores)
+
+# Write simulated data to files:
+sparse.save_npz("../data/dms_simulation_150000_variants.npz", bmap.binary_variants)
+np.savetxt("../data/dms_simulation_150000_variants_funcscores.txt", bmap.func_scores,
+        delimiter='\t')
