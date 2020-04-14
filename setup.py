7c137e8898443030578905a42cf3590052c0ca2d
Erick Matsen <ematsen@gmail.com>
2020-04-14 10:44:13 -0700

    first steps to package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="torchdms",
+    version="0.0.0",
+    description="Tools for analyzing DMS data using PyTorch",
+    url="http://github.com/storborg/funniest",
+    author="Matsen group",
+    author_email="ematsen@gmail.com",
+    license="MIT",
+    packages=["torchdms"],
+    zip_safe=False,
+)
