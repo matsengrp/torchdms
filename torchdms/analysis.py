10ef4daa7a02309986b20321fae6bf8963f57990
Erick Matsen <ematsen@gmail.com>
2020-04-19 04:48:50 -0700

    Generalizing to allow a list of data sets.

--- torchdms/analysis.py
+++ torchdms/analysis.py
@@ -17,32 +17,41 @@ def make_data_loader_infinite(data_loader):
 
 
 class Analysis:
-    def __init__(self, model, train_data):
+    def __init__(self, model, train_data_list):
         self.learning_rate = 1e-3
         self.batch_size = 5000
         self.model = model
-        self.train_dataset = BinarymapDataset(train_data)
-        self.train_dataloader = DataLoader(
-            self.train_dataset, batch_size=self.batch_size, shuffle=True
-        )
-        self.train_infinite_dataloader = make_data_loader_infinite(
-            self.train_dataloader
-        )
+        self.train_datasets = [
+            BinarymapDataset(train_data) for train_data in train_data_list
+        ]
+        self.train_loaders = [
+            DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+            for train_dataset in self.train_datasets
+        ]
+        self.train_infinite_loaders = [
+            make_data_loader_infinite(train_loader)
+            for train_loader in self.train_loaders
+        ]
         self.optimizer = torch.optim.Adam(model.parameters(), lr=self.learning_rate)
         self.losses = []
 
     def train(self, criterion, epoch_count):
         self.losses = []
+        batch_count = 1 + max(map(len, self.train_datasets)) // self.batch_size
         scheduler = ReduceLROnPlateau(self.optimizer, patience=5, verbose=True)
         self.model.train()  # Sets model to training mode.
         for _ in range(epoch_count):
             total_loss = 0
-            batch_count = 1 + len(self.train_dataset) // self.batch_size
             for _ in range(batch_count):
                 self.optimizer.zero_grad()
-                batch = next(self.train_infinite_dataloader)
-                outputs = self.model(batch["variants"])
-                loss = criterion(outputs.squeeze(), batch["func_scores"]).sqrt()
+                per_loader_losses = torch.zeros(len(self.train_infinite_loaders))
+                for i, train_infinite_loader in enumerate(self.train_infinite_loaders):
+                    batch = next(train_infinite_loader)
+                    outputs = self.model(batch["variants"])
+                    per_loader_losses[i] = criterion(
+                        outputs.squeeze(), batch["func_scores"]
+                    ).sqrt()
+                loss = torch.sum(per_loader_losses)
                 self.losses.append(loss.item())
                 loss.backward()
                 self.optimizer.step()
