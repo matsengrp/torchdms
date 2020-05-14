3c31f10273dfc34f070d75bf1b0f5233b1d88308
Erick Matsen <ematsen@gmail.com>
2020-05-14 09:50:39 -0700

    simpler run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,9 @@
-set -eux
+set -eu
 
-run() {
-    FLAGS=$@
-    for CONFIG in $(find $PWD -name "config.json")
-    do
-        cd $(dirname $CONFIG)
-        tdms $FLAGS go --config $CONFIG
-    done
-}
-
-run --dry-run
-
-read -p "Do want to execute this run? [n] " yn
-case $yn in
-    [Yy]* ) run; break;;
-    * ) break;;
-esac
+for CONFIG in $(find $PWD -name "config.json")
+do
+    cd $(dirname $CONFIG)
+    echo "\n\n*** Starting $CONFIG"
+    tdms go --config $CONFIG
+    echo "*** $CONFIG ran successfully"
+done
