Adding a sign to the monotonic option; fixes (#45)
* Warning if "library" isn't used for split.
* Passing a copy of groupby to prep_by_stratum_and_export
* adding "default" key to config_dict.
* monotonic_sign controls the sign of the monotonicity

Closes #41

torchdms/data.py
@@ -1,15 +1,13 @@
 """
 Tools for handling data.
 """
-
-import pickle
-import torch
-from dms_variants.binarymap import BinaryMap
-from torch.utils.data import Dataset
 from collections import defaultdict
 import itertools
 import random
 import click
+import torch
+from torch.utils.data import Dataset
+from dms_variants.binarymap import BinaryMap
 from torchdms.utils import (
     to_pickle_file,
     make_legal_filename,
@@ -139,8 +137,8 @@ def prep_by_stratum_and_export(
     for train_part in partitioned_train_data:
         num_subs = len(train_part["aa_substitutions"][0].split())
         click.echo(
-            f"LOG: There are {len(train_part)} training examples \
-              for stratum: {num_subs}"
+            f"LOG: There are {len(train_part)} training examples "
+            f"for stratum: {num_subs}"
         )
 
     click.echo(f"LOG: There are {len(test_partition)} test points")

torchdms/model.py
@@ -26,11 +26,11 @@ class VanillaGGE(nn.Module):
         layers,
         output_size,
         activation_fn=torch.sigmoid,
-        monotonic=False,
+        monotonic_sign=None,
         beta_l1_coefficient=0.0,
     ):
         super(VanillaGGE, self).__init__()
-        self.monotonic = monotonic
+        self.monotonic_sign = monotonic_sign
         self.input_size = input_size
         self.output_size = output_size
         self.layers = []
@@ -68,7 +68,7 @@ class VanillaGGE(nn.Module):
         PyTorch description.
         """
         return {
-            "monotonic": self.monotonic,
+            "monotonic": self.monotonic_sign,
             "activation_fn": self.activation_fn,
             "beta_l1_coefficient": self.beta_l1_coefficient,
         }
@@ -78,10 +78,12 @@ class VanillaGGE(nn.Module):
 
     def forward(self, x):
         out = x
-        for layer_index in range(len(self.layers) - 1):
-            out = self.activation_fn(getattr(self, self.layers[layer_index])(out))
-        prediction = getattr(self, self.layers[-1])(out)
-        return prediction
+        for layer_name in self.layers[:-1]:
+            out = self.activation_fn(getattr(self, layer_name)(out))
+        out = getattr(self, self.layers[-1])(out)
+        if self.monotonic_sign:
+            out *= self.monotonic_sign
+        return out
 
     def regularization_loss(self):
         """