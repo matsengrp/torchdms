a9589bd72bf7c65ef5ead0f4d7c6dca0121fb426
zorian15 <zoriant15@gmail.com>
2020-10-21 08:02:24 -0700

    target exponentiation for lasso and mse loss

--- torchdms/loss.py
+++ torchdms/loss.py
@@ -2,7 +2,7 @@
 import torch
 
 
-def l1(y_true, y_predicted, loss_decay=None):
+def l1(y_true, y_predicted, loss_decay=None, exp_target=None):
     """Mean square error, perhaps with loss decay."""
     if loss_decay:
         y_true_squoze = y_true.squeeze()
@@ -10,11 +10,15 @@ def l1(y_true, y_predicted, loss_decay=None):
             torch.exp(loss_decay * y_true_squoze)
             * (y_true_squoze - y_predicted.squeeze()).abs()
         )
+    elif exp_target:
+        y_true_squoze = np.pow(exp_target,y_true.squeeze())
+        y_predicted_squoze = np.pow(exp_target, y_predicted.squeeze())
+        return torch.nn.functional.mse_loss(y_true_exp, y_predicted_exp)
     # else:
     return torch.nn.functional.l1_loss(y_true.squeeze(), y_predicted.squeeze())
 
 
-def mse(y_true, y_predicted, loss_decay=None):
+def mse(y_true, y_predicted, loss_decay=None, exp_target=None):
     """Mean square error, perhaps with loss decay."""
     if loss_decay:
         y_true_squoze = y_true.squeeze()
@@ -22,10 +26,14 @@ def mse(y_true, y_predicted, loss_decay=None):
             torch.exp(loss_decay * y_true_squoze)
             * (y_true_squoze - y_predicted.squeeze()) ** 2
         )
+    elif exp_target:
+        y_true_squoze = np.pow(exp_target,y_true.squeeze())
+        y_predicted_squoze = np.pow(exp_target, y_predicted.squeeze())
+        return torch.nn.functional.mse_loss(y_true_exp, y_predicted_exp)
     # else:
     return torch.nn.functional.mse_loss(y_true.squeeze(), y_predicted.squeeze())
 
 
-def rmse(y_true, y_predicted, loss_decay=None):
+def rmse(y_true, y_predicted, loss_decay=None, exp_target=None):
     """Root mean square error, perhaps with loss decay."""
     return mse(y_true, y_predicted, loss_decay).sqrt()
