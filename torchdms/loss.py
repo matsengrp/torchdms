93121d042742bf400da9f551115527b108ebcd50
zorian15 <zoriant15@gmail.com>
2020-10-29 10:11:28 -0700

    linting and black

--- torchdms/loss.py
+++ torchdms/loss.py
@@ -4,17 +4,22 @@ import torch
 
 def make_squoze_y_pair(y_true, y_predicted, exp_target):
     """
-    Make squeezed versions of y_true and y_predicted, perhaps using exp_target as a base for exponentiation.
+    Make squeezed versions of y_true and y_predicted, perhaps using exp_target
+    as a base for exponentiation.
     """
     if exp_target is not None:
-        return torch.pow(exp_target, y_true.squeeze()), torch.pow(exp_target, y_predicted.squeeze())
+        return torch.pow(exp_target, y_true.squeeze()), torch.pow(
+            exp_target, y_predicted.squeeze()
+        )
     # else:
     return y_true.squeeze(), y_predicted.squeeze()
 
 
 def l1(y_true, y_predicted, loss_decay=None, exp_target=None):
     """Mean square error, perhaps with loss decay."""
-    y_true_squoze, y_predicted_squoze = make_squoze_y_pair(y_true, y_predicted, exp_target)
+    y_true_squoze, y_predicted_squoze = make_squoze_y_pair(
+        y_true, y_predicted, exp_target
+    )
     if loss_decay:
         return torch.sum(
             torch.exp(loss_decay * y_true_squoze)
@@ -26,7 +31,9 @@ def l1(y_true, y_predicted, loss_decay=None, exp_target=None):
 
 def mse(y_true, y_predicted, loss_decay=None, exp_target=None):
     """Mean square error, perhaps with loss decay."""
-    y_true_squoze, y_predicted_squoze = make_squoze_y_pair(y_true, y_predicted, exp_target)
+    y_true_squoze, y_predicted_squoze = make_squoze_y_pair(
+        y_true, y_predicted, exp_target
+    )
     if loss_decay:
         return torch.sum(
             torch.exp(loss_decay * y_true_squoze)
