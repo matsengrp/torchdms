970ee382860b65174b47bc8cce221bc1e52dabb7
Erick Matsen <ematsen@gmail.com>
2021-01-25 11:23:16 -0800

    group_lasso and test

--- torchdms/loss.py
+++ torchdms/loss.py
@@ -1,4 +1,4 @@
-"""Loss functions."""
+"""Loss functions and functions relevant to losses."""
 import torch
 
 
@@ -45,3 +45,13 @@ def mse(y_true, y_predicted, loss_decay=None, exp_target=None):
 def rmse(y_true, y_predicted, loss_decay=None):
     """Root mean square error, perhaps with loss decay."""
     return mse(y_true, y_predicted, loss_decay).sqrt()
+
+
+def group_lasso(matrix):
+    """The sum of the 2-norm across columns."""
+    shape = list(matrix.shape)
+    assert len(shape) == 2
+    total = torch.Tensor([0.0])
+    for i in range(shape[1]):
+        total += matrix[:, i].norm(2)
+    return total
--- torchdms/test/test_loss.py
+++ torchdms/test/test_loss.py
@@ -3,7 +3,8 @@ Testing for loss.py.
 """
 from math import exp
 from torch import Tensor
-from torchdms.loss import l1, mse
+from torchdms.loss import l1, mse, group_lasso
+import numpy as np
 
 
 def test_mse_loss():
@@ -22,3 +23,10 @@ def test_l1_loss():
     y_predicted = Tensor([0.2, 9.0])
     correct_decayed_loss = exp(0.3 * 0.1) * (0.2 - 0.1) + exp(0.3 * 4.0) * (9.0 - 4.0)
     assert l1(y_true, y_predicted, 0.3) == correct_decayed_loss
+
+
+def test_group_lasso():
+    """Test that the group lasso is giving the expected quantities."""
+    matrix = Tensor([[0.1, 3.0], [0.2, 4.0]])
+    correct = np.sqrt(0.1 * 0.1 + 0.2 * 0.2) + np.sqrt(3.0 * 3.0 + 4.0 * 4.0)
+    assert group_lasso(matrix) == correct
