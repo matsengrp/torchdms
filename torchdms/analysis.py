75e045d3f83df2ffe80de8ed63fa6b09208d3f61
Zorian Thornton <zoriant15@gmail.com>
2021-03-05 14:04:44 -0800

    96 mutation track (#105)
    * enable tracking of unseen mutations in training data
    
    * functionality for creating a set of all possible aa-subs in seq-space
    
    * analysis object uses train data to find unseen muts. Stores in model.
    
    * handle unseen mutations during analysis class initialization.
    
    * formatting
    
    * add assertions for mutation map creation
    
    * formatting.
    
    * spiffying things up -- resolving PR comments by @matsen
    
    * formating
    
    * updated ignore
    
    * updated ignore
    
    * cleaning

--- torchdms/analysis.py
+++ torchdms/analysis.py
@@ -7,7 +7,7 @@ import torch
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torchdms.data import BinaryMapDataset
-from torchdms.utils import build_beta_map
+from torchdms.utils import build_beta_map, make_all_possible_mutations
 
 
 def make_data_loader_infinite(data_loader):
@@ -58,6 +58,22 @@ class Analysis:
             for train_loader in self.train_loaders
         ]
         self.val_loss_record = sys.float_info.max
+        self.all_possible_mutations = make_all_possible_mutations(self.val_data)
+        self.set_unseen_training_mutations()
+
+    def set_unseen_training_mutations(self):
+        """Store unseen training mutations in model."""
+        observed_mutations = set()
+        for train_dataset in self.train_datasets:
+            train_muts = train_dataset.original_df["aa_substitutions"]
+            train_muts_split = [sub for muts in train_muts for sub in muts.split()]
+            observed_mutations.update(train_muts_split)
+        self.model.set_unseen_mutations(
+            self.all_possible_mutations.difference(observed_mutations)
+        )
+        assert len(self.model.unseen_mutations) + len(observed_mutations) == len(
+            self.all_possible_mutations
+        ), "Unseen and observed mutation numbers don't add up!"
 
     def loss_of_targets_and_prediction(
         self, loss_fn, targets, predictions, per_target_loss_decay
--- torchdms/utils.py
+++ torchdms/utils.py
@@ -196,3 +196,22 @@ def build_beta_map(test_data, beta_vec):
         beta_vec.reshape(len(test_data.wtseq), len(bmap.alphabet)).transpose(),
         alphabet,
     )
+
+
+def make_all_possible_mutations(test_data):
+    """This function creates a set of all possible amino acid substitutions.
+
+    Takes a data set (preferably the testing data). Returns a 20*L list
+    of possible mutations.
+    """
+    wtseq = test_data.wtseq
+    all_possible_mutations = [
+        wt_aa + str(site + 1) + alt_aa
+        for site, wt_aa in enumerate(wtseq)
+        for alt_aa in test_data.alphabet
+        if alt_aa != wt_aa
+    ]
+    # make sure all mutations from WT are stored.
+    assert len(all_possible_mutations) == (len(test_data.alphabet) - 1) * len(wtseq)
+    assert len(set(all_possible_mutations)) == len(all_possible_mutations)
+    return set(all_possible_mutations)
