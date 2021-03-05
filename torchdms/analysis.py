0a058f9e87fbdd94234dbabe94bac144083ea989
zorian15 <zoriant15@gmail.com>
2021-03-05 12:46:43 -0800

    cleaning

--- torchdms/analysis.py
+++ torchdms/analysis.py
@@ -63,18 +63,17 @@ class Analysis:
 
     def set_unseen_training_mutations(self):
         """Store unseen training mutations in model."""
-        if self.model.unseen_mutations is None:
-            observed_mutations = set()
-            for train_dataset in self.train_datasets:
-                train_muts = train_dataset.original_df["aa_substitutions"]
-                train_muts_split = [sub for muts in train_muts for sub in muts.split()]
-                observed_mutations.update(train_muts_split)
-            self.model.set_unseen_mutations(
-                self.all_possible_mutations.difference(observed_mutations)
-            )
-            assert len(self.model.unseen_mutations) + len(observed_mutations) == len(
-                self.all_possible_mutations
-            ), "Unseen and observed mutation numbers don't add up!"
+        observed_mutations = set()
+        for train_dataset in self.train_datasets:
+            train_muts = train_dataset.original_df["aa_substitutions"]
+            train_muts_split = [sub for muts in train_muts for sub in muts.split()]
+            observed_mutations.update(train_muts_split)
+        self.model.set_unseen_mutations(
+            self.all_possible_mutations.difference(observed_mutations)
+        )
+        assert len(self.model.unseen_mutations) + len(observed_mutations) == len(
+            self.all_possible_mutations
+        ), "Unseen and observed mutation numbers don't add up!"
 
     def loss_of_targets_and_prediction(
         self, loss_fn, targets, predictions, per_target_loss_decay
