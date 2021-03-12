2c5296aa78a5ca7bdf7cb8a9a534d36898bdf484
zorian15 <zoriant15@gmail.com>
2021-03-11 18:01:23 -0800

    formatting

--- torchdms/test/test_data.py
+++ torchdms/test/test_data.py
@@ -74,11 +74,6 @@ def test_wt_idx():
         export_dataframe=None,
         partition_label=None,
     )
-    split_df_prepped = SplitDataset.of_split_df(
-        split_df,
-        wtseq,
-        ['affinity_score'],
-        ''
-    )
+    split_df_prepped = SplitDataset.of_split_df(split_df, wtseq, ["affinity_score"], "")
     assert split_df_prepped.val.wtseq == "NIT"
     assert torch.all(torch.eq(actual_idx, split_df_prepped.val.wt_idxs))
